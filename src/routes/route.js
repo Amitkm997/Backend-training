80314cdc1a1f594b0aab183547369e8a000d9520
Amitkm997 <amitkm997@gmail.com>
2022-08-06 10:53:08 +0530

    The code of routes is being modified

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,31 @@
 const express = require('express');
+//linking module -1
 const abc = require('../introduction/intro')
+//linking module -2
+const dat=require('../util/helper.js')
+//linking module -3
+const valid=require('../validator/formatter.js')
+
+const log=require('../logger/logger.js')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
+    //module 1
+    log.name
+    log.logIn()
+    //module 2
+    dat.printDate();
+    dat.printmontht();
+    dat.datt();
+
+    //module 3
+    valid.trim();
+    valid.chagetoLowerCase();
+    valid.chagetoUpperCase();
+
+
     res.send('My second ever api!')
 });
 

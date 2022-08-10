b68dafd010fe784c53380d619fa234fa62bf9b52
Amitkm997 <amitkm997@gmail.com>
2022-08-10 13:19:03 +0530

    Completed Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,56 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+//assignment
+// const express = require('express');
+// const router = express.Router();
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+
+       //LOGIC WILL COME HERE
+       
+       for (let i=0;i<players.length;i++){
+        if(players[i].name==req.body.name){
+             res.send("Your name already exit so you are not allowed to enter your details");
+        }
+        }
+        players.push(req.body)
+            res.send(  { data: players , status: true }  )
+    
+   })
+  
+
+
+
 module.exports = router;

3850816f2c52446195c3b049b0eca4f8fe7ba30d
Amitkm997 <amitkm997@gmail.com>
2022-08-25 19:39:09 +0530

    this is the assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,22 +2,30 @@ const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+// const BookController= require("../controllers/bookController")
+// const commonMW = require ("../middlewares/commonMiddlewares")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-
-
-
-router.post("/createBook", BookController.createBook  )
-
-
-
-
+//assignment
 router.post("/createUser", UserController.createUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.post("/createBook", BookController.createBook  )
 // router.get("/getUsersData", UserController.getUsersData)
 
 
@@ -48,7 +56,7 @@ router.post("/createUser", UserController.createUser)
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+// router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 

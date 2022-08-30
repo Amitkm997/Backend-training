f3bbc46f183af75b9bbf1567fa42c396e9f54807
Amitkm997 <amitkm997@gmail.com>
2022-08-31 00:34:17 +0530

    this is the assignemnet soloutjion of try and catch block

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,28 +5,41 @@ const authenticate = function (req, res, next) {
     //check the token in request header
     //validate this token
     //   let token = req.headers["x-Auth-token"] //let token = req.headers["x-Auth-token"];
+    try {
 
-     let token = req.headers['x-Auth-token'];
-    if (!token) token = req.headers['x-auth-token'];//case senstive
-    if (!token) return res.send({ status: false, msg: "token must be present" })
-    console.log(token);
-    let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-     if (!decodedToken){
-   return res.send({ status: false, msg: "token is invalid" });
+        let token = req.headers['x-Auth-token'];
+        if (!token) token = req.headers['x-auth-token'];//case senstive
+        if (!token) return res.status(400).send({ status: false, msg: "token must be present" })
+        console.log(token);
+        let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+        if (!decodedToken) {
+            return res.status(401).send({ status: false, msg: "token is invalid" });
+        }
+        req.loggedInUser = decodedToken.userId
+        next()
+    } catch (error) {
+        console.log("This is the error", error);
+        res.status(400).send({ msg: "please check your input", error: error })
     }
-    req.loggedInUser = decodedToken.userId
-    next()
+
+
 }
 
 
 const authorise = function (req, res, next) {
-    // comapre the logged in user's id and the id in request
-    let userToBeModified = req.params.userId//user access userId 
-    //let userloggedin = jwkdecoder.userId//decorder userId
-    if (userToBeModified !=req.loggedInUser) {
-        res.send({ status: false, msg: "userLoggedIn is not allowed " })
+    try {
+        // comapre the logged in user's id and the id in request
+        let userToBeModified = req.params.userId//user access userId 
+        //let userloggedin = jwkdecoder.userId//decorder userId
+        if (userToBeModified != req.loggedInUser) {
+            res.status(403).send({ status: false, msg: "userLoggedIn is not allowed " })
+        }
+        next()
+    } catch (error) {
+        console.log("This is the error", error);
+        res.status(400).send({ msg: "please check your input", error: error })
     }
-    next()
+
 }
 
 module.exports.authenticate = authenticate
